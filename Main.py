f55b227199b0a27f6fc8829ae90dce660822a2ad
Kanin (Laptop) <kanin.mann00@gmail.com>
2020-05-24 14:42:23 -0400

    Lunch break #1

--- Main.py
+++ Main.py
@@ -17,6 +17,7 @@ SCALE = 200
 # Inits pygame and gets the app all up and running
 if(init((SIZE[0]*SCALE, SIZE[1]*SCALE), APP_NAME)):
     Global.running = True
+    Log("Game is running!")
 
 # Update method called every frame
 def update():
@@ -40,6 +41,6 @@ def quit():
 # Prob not best practice but I find it useful in that I can just zoom to the
 # end and find the loop.
 while(Global.running):
-    if(Global.shut_down): quit()
+    if(Global.shut_down_request): quit()
     update()
     fixed_update()
