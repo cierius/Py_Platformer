8180b079f4f9935bc516c454b3ba868f12def3d5
Kanin (desktop) <kanin.mann00@gmail.com>
2020-05-24 21:27:37 -0400

    Desktop commit #1

--- Graphics.py
+++ Graphics.py
@@ -1,10 +1,10 @@
 import Global
 import pygame
 
-from Global import Color
+from Global import Color, player
 
 def graphics():
-    pygame.draw.rect(Global.screen, Color.get("Teal"), (15, Global.test_y, 100, 100))
+    player.render()
 
     pygame.display.flip() # flip swaps the buffer canvas
     Global.screen.fill(Color.get("Black"))
--- Main.py
+++ Main.py
@@ -1,33 +1,37 @@
 import pygame
-import Global
 import sys
+import time
+
+# My scripts below this
+import Global
 
 from Logger import *
 from Init import init
 from Event_Handler import event_handler
 from Graphics import graphics
+from Global import player
 
 
 # Constants
 APP_NAME = "Py_Platformer"
-SIZE = (3, 4)
+SIZE = (4, 3)
 SCALE = 200
 
-
-# Inits pygame and gets the app all up and running
-if(init((SIZE[0]*SCALE, SIZE[1]*SCALE), APP_NAME)):
-    Global.running = True
-    Log("Game is running!")
 
 # Update method called every frame
 def update():
     event_handler()
     graphics()
 
+
 # Update method called 60 times per sec.
 # Use for game mechanics.
 def fixed_update():
-    pass
+    if(Global.move_left):
+        player.move("Left")
+    if(Global.move_right):
+        player.move("Right")
+
 
 def quit():
     Log("Closing Application")
@@ -36,11 +40,35 @@ def quit():
     sys.exit()
 
 
-
 # I always have my main loop at the very bottom of my code.
 # Prob not best practice but I find it useful in that I can just zoom to the
 # end and find the loop.
-while(Global.running):
-    if(Global.shut_down_request): quit()
-    update()
-    fixed_update()
+def main():
+    # variables for frame rate limitation
+    start_frame = 0
+    delta_time = 0
+    frame_time = 0
+
+    # Inits pygame and gets the app all up and running
+    if(init((SIZE[0]*SCALE, SIZE[1]*SCALE), APP_NAME)):
+        Global.running = True
+        Log("Game is running!")
+
+    while(Global.running):
+        # Gets time at beginning of frame
+        start_frame = time.time()
+
+        if(Global.shut_down_request): quit()
+        update()
+
+        # Gets time at end of frame and then finds the diff
+        delta_time = time.time() - start_frame
+        frame_time += delta_time
+
+        if(frame_time >= 0.0166): # 1 second divided by 60 frames = 0.0166...
+            fixed_update()
+            frame_time -= 0.0166
+
+
+if __name__ == '__main__':
+    main()
